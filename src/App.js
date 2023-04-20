204046e0789ea3a5ca28f70f2930d80184b20b08
Gerald <53179363+Gercai@users.noreply.github.com>
2023-04-20 15:53:02 +0100

    basic header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Header from './components/header';
+
 
 function App() {
   return (
     <div className="App">
-      hallo
-     
+     <Header></Header>
     </div>
   );
 }
---
+++ src/components/main.js
@@ -0,0 +1,13 @@
+export default () => {
+    return(
+        <div className="header">
+            <h1>Art Gallery</h1>
+        <nav className="navBar"><ul>
+            <li>Home</li>
+            <li>Malen</li>
+            <li>Sign In</li>
+            <li>Sign Up</li>A
+            </ul></nav>
+        </div>
+    )
+}
