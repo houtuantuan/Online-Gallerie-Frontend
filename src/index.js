31593a7069bdbf54555e6a1f876a0c4c1db3fbee
Gerald <53179363+Gercai@users.noreply.github.com>
2023-04-20 13:54:56 +0100

    added fonts

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import "./css/index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
