47c53e1446b4e7f3affe60f955cc7c337c0b53c9
Gerald <53179363+Gercai@users.noreply.github.com>
2023-04-24 19:58:19 +0100

    merge conflicts done

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import ExploreTime from './components/ExploreTime'
 import Overview from "./components/Overview"
 import Header from './components/header'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
-import theme from './util/themes'
-import CanvasComp from './components/CanvasComp'
+import theme from './muiutils/themes'
+import CanvasComp from './components/CanvasInterface'
 import { Routes, Route } from 'react-router-dom'
 
 function App () {
@@ -19,7 +19,6 @@ function App () {
           <Route path="gallery" element={<Overview/>} />
         </Routes>
         <ExploreTime theme={theme}></ExploreTime>{' '}
-        
       </ThemeProvider>
     </div>
   )
