9bb5d2a8f85a94b7a864c75758a204346831dff5
Andreas L <114237284+Sinnfull85@users.noreply.github.com>
2023-04-26 13:41:00 +0200

    update App + index.js

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
-import './css/index.scss'
-import { BrowserRouter } from 'react-router-dom'
-import ReactCardSlider from "./components/CardSlider"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import "./css/index.scss";
+import {BrowserRouter} from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
     <App />
-  </BrowserRouter>
+    </BrowserRouter>
+  </React.StrictMode>
 );
-const slides = ReactDOM.createRoot(document.getElementById('carousel'))
+
+
+const slides = ReactDOM.createRoot(document.getElementById('carousel'));
 slides.render(
     <BrowserRouter>
       <ReactCardSlider />
     </BrowserRouter>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
