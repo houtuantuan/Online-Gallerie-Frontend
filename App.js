44275ea2b6d8dc795b7fe5689615d3320c28295a
Andreas L <114237284+Sinnfull85@users.noreply.github.com>
2023-04-26 13:04:49 +0200

    update App + index.js

---
+++ App.js
@@ -0,0 +1,34 @@
+import './App.css'
+import ExploreTime from './components/ExploreTime'
+import Overview from "./components/Overview"
+import Header from './components/header'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import theme from './muiutils/themes'
+import CanvasComp from './components/CanvasInterface'
+import { Routes, Route } from 'react-router-dom'
+import ReactCardSlider from 'react-card-slider-component'
+
+function App () {
+ 
+  return (
+
+    <><div className='App'>
+      <ThemeProvider theme={theme}>
+        <Header></Header>
+        <Routes>
+          <Route path='canvas' element={<CanvasComp />} />
+          <Route path="gallery" element={<Overview />} />
+        </Routes>
+        {/* <ExploreTime theme={theme}></ExploreTime>{' '} */}
+      </ThemeProvider>
+    </div>
+    <p />
+    <div className='Carousel'>
+        <ThemeProvider theme={theme}>
+          <ReactCardSlider theme={theme}></ReactCardSlider>{' '}
+        </ThemeProvider>
+      </div></>
+  )
+}
+
+export default App
