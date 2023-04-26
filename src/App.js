a551dd6a3190a5b96b6d402f05c411b638a4a8ed
houtuantuan <80598603+houtuantuan@users.noreply.github.com>
2023-04-26 13:10:34 +0200

    new function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css'
-import ExploreTime from './components/ExploreTime'
 import Overview from "./components/Overview"
 import Header from './components/header'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 import theme from './muiutils/themes'
 import CanvasComp from './components/CanvasInterface'
 import { Routes, Route } from 'react-router-dom'
+import HomePage from './components/HomePage'
+import SingleImage from './components/SingleImage'
 
 function App () {
  
@@ -15,10 +16,13 @@ function App () {
       <ThemeProvider theme={theme}>
         <Header></Header>
         <Routes>
+          <Route path="" element={<HomePage/>}/>
           <Route path='canvas' element={<CanvasComp />} />
           <Route path="gallery" element={<Overview/>} />
+          <Route path="gallery/:_id" element={<SingleImage/>} />
+
         </Routes>
-        {/* <ExploreTime theme={theme}></ExploreTime>{' '} */}
+        
       </ThemeProvider>
     </div>
   )
